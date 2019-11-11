20b31a2c3ffa8606d167f530e5eb315e7d6142f5
tv <volianskyitv@gmail.com>
2019-11-11 02:52:10 +0200

    refactoring of the code

--- app/src/main/java/com/example/notificationmanager/MainActivity.java
+++ app/src/main/java/com/example/notificationmanager/MainActivity.java
@@ -145,10 +145,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
          animation = new TranslateAnimation(0, 0, 0, 550);
         animation.setDuration(1000);
         animation.setFillAfter(true);
-        imageViewStartPlay.startAnimation(animation);
+     //   imageViewStartPlay.startAnimation(animation);
 
-        frameLayout.startAnimation(animation);
         videoView.startAnimation(animation);
+        frameLayout.startAnimation(animation);
 
     }
 
